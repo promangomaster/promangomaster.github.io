add0ca7e04f04d887065ecfdf7eaecea9c9027da
promangomaster <110459369+promangomaster@users.noreply.github.com>
2022-12-18 18:35:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.modal__enter').on('click', function(e) {
+        e.preventDefault();
+        $('.overlay, #welcome').fadeOut('slow');
+    });
+
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
